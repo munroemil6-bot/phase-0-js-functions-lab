0592ed92ec743b6886d1cd08d31c4fe91bacfdcb
myles <munroemil6@gmail.com>
2026-03-26 23:25:22 +0300

    functions completion

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+function calculateTax(amount) {
+    let tax = amount * 0.1;
+    return tax;
+}
+function convertToUpperCase() {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word) {
+    const cleanedWord = word.replace(/[a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedWord = cleanedWord.split('').reverse.join('');
+    return cleanedWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPecentage) {
+    const discountAmount = (originalPrice * discountPecentage) / 100;
+    const discountedPrice = originalPrice - discountAmount;
+    return discountedPrice;
+}
 
 
 
